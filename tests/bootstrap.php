15e5f17426a9659f6ca49454d8f174effb881cc2
William DURAND <william.durand1@gmail.com>
2012-04-20 13:59:42 +0200

    Switched to composer for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,3 @@
 <?php
 
-use Symfony\Component\ClassLoader\UniversalClassLoader;
-
-require_once __DIR__ . '/../vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-
-$loader = new UniversalClassLoader;
-$loader->registerNamespaces(array(
-    'OAuth2' => __DIR__.'/../lib',
-    'Symfony' => __DIR__.'/../vendor',
-));
-
-$loader->register();
-
+require __DIR__ . '/../vendor/autoload.php';
